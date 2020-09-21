2856f7d90be1ccca084cb10f42a2a8a9db34b1c6
yunhong-0512 <hongyun0512@hotmail.com>
2020-09-21 14:28:04 +0800

    completed hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,87 @@
+
+package synthesizer;
+
+import java.util.Iterator;
+
+
+public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
+        rb = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+        rb[last] = x;
+        fillCount += 1;
+        last = (last + 1) % capacity;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        T res = rb[first];
+        fillCount -= 1;
+        first = (first + 1) % capacity;
+        return res;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        return rb[first];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new BufferIterator<T>();
+    }
+
+    private class BufferIterator<T> implements Iterator<T> {
+        int ptr = first;
+        int size = fillCount;
+        public boolean hasNext() {
+            return size != 0;
+        }
+
+        public T next() {
+            size -= 1;
+            T res = (T) rb[ptr];
+            ptr = (ptr + 1) % capacity;
+            return res;
+        }
+    }
+}
