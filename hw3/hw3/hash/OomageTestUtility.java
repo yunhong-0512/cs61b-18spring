23aa53677d6a1c148482d954f7abda7058039d9d
yunhong-0512 <hongyun0512@hotmail.com>
2020-09-24 14:56:06 +0800

    completed hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,17 +1,24 @@
 package hw3.hash;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
-         * Write a utility function that returns true if the given oomages
-         * have hashCodes that would distribute them fairly evenly across
-         * M buckets. To do this, convert each oomage's hashcode in the
-         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
-         * and ensure that no bucket has fewer than N / 50
-         * Oomages and no bucket has more than N / 2.5 Oomages.
-         */
-        return false;
+        int N = oomages.size();
+        LinkedList<Oomage>[] buckets = new LinkedList[M];
+        for (int i = 0; i < M; i++) {
+            buckets[i] = new LinkedList<Oomage>();
+        }
+        for (int i = 0; i < N; i++) {
+            int bucketNum = (oomages.get(i).hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum].add(oomages.get(i));
+        }
+        for (int i = 0; i < M; i++) {
+            if (buckets[i].size() < N / 50 || buckets[i].size() > N / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,10 +1,12 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TestComplexOomage {
@@ -33,18 +35,18 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
-     * that shows the flaw in the hashCode function.
-     */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
+        int N = 10000;
 
-        // Your code here.
+        for (int i = 0; i < N; i += 1) {
+            deadlyList.add(ComplexOomage.randomComplexOomage());
+        }
 
-        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 256));
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
